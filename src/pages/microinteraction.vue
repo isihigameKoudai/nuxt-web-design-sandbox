11f5ebc7e3951b880b8d10af221b743e0d12b2e7
koudaiishigame <kira5052@gmail.com>
2020-08-13 14:31:44 +0900

    全pages追加

---
+++ src/pages/microinteraction.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="index-page">
+    <h1 class="title">nuxt</h1>
+    <p class="text">hello</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+<style lang="scss" scoped>
+.index-page {
+  .title {
+    font-size: 2.4rem;
+  }
+}
+</style>
