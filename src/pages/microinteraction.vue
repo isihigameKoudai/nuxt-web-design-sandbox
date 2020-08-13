6247af3a50c2fb7aaac5f9ed201dc7b8653c5ab2
koudaiishigame <kira5052@gmail.com>
2020-08-13 16:21:22 +0900

    microintaraction page初期設定

--- src/pages/microinteraction.vue
+++ src/pages/microinteraction.vue
@@ -1,19 +1,60 @@
 <template>
-  <div class="index-page">
-    <h1 class="title">nuxt</h1>
-    <p class="text">hello</p>
+  <div class="microintaraction-page">
+    <article class="wrap">
+      <section>
+        <LoadingContainer>
+          <WaveBar />
+        </LoadingContainer>
+      </section>
+    </article>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { defineComponent } from '@vue/composition-api'
 
-export default Vue.extend({})
+import LoadingContainer from '@/components/LoadingContainer'
+import WaveBar from '@/components/LoadingIcons/WaveBar'
+
+export default defineComponent({
+  components: {
+    LoadingContainer,
+    WaveBar,
+  },
+})
 </script>
+
 <style lang="scss" scoped>
-.index-page {
-  .title {
-    font-size: 2.4rem;
+.microintaraction-page {
+  animation: bg-color 5s ease 0s infinite forwards;
+  min-height: 100vh;
+
+  .wrap {
+    width: 1024px;
+    height: auto;
+    margin: 0 auto;
+    padding: 50px 0;
+  }
+}
+
+@keyframes bg-color {
+  0% {
+    background-color: #e74c3c;
+  }
+  20% {
+    background-color: #f1c40f;
+  }
+  40% {
+    background-color: #1abc9c;
+  }
+  60% {
+    background-color: #3498db;
+  }
+  80% {
+    background-color: #9b59b6;
+  }
+  100% {
+    background-color: #e74c3c;
   }
 }
 </style>
